c8e9c8f8a4ea77f7fb774941f631bdd6ef87e25d
missferret <dana@streamelements.com>
2020-04-03 19:19:53 -0700

    [FEAT] Title styling

--- frontend/src/components/report/Report.js
+++ frontend/src/components/report/Report.js
@@ -9,6 +9,12 @@ const styles = {
   container: {
     height: '100%',
   },
+  reportTitle: {
+    color: '#90A1B5',
+  },
+  centerName: {
+    marginBottom: '25px !important',
+  },
   buttonContainer: {
     display: 'flex',
     justifyContent: 'center',
@@ -26,10 +32,9 @@ export const Report = ({ medicalCenter, onReportSubmit }) => {
   } = medicalCenter;
   return (
     <div css={styles.container}>
-      <Typography>דוח יומי:</Typography>
-      <Typography>
-        {id}
-        {name}
+      <Typography variant='h6' css={styles.reportTitle}>דוח יומי:</Typography>
+      <Typography variant='h5' css={styles.centerName}>
+        {id}&nbsp; <strong>{name}</strong>
       </Typography>
       {
         departments.map(department => (
